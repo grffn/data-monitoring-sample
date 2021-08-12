0d3e495bdb16cbdbc9b1b5bc194185705cb67d4c
Grigorii Hait <grffn.94@gmail.com>
2021-08-12 16:41:35 +0300

    Add MongoDb storage service

---
+++ src/DataMonitoring.MongoDb/MongoDbPositionService.cs
@@ -0,0 +1,32 @@
+﻿using DataMonitoring.Core;
+using DataMonitoring.Core.Models;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataMonitoring.Storage.MongoDb
+{
+    public class MongoDbPositionService : IPositionService
+    {
+        private readonly IMongoCollection<AssetPosition> _positionsCollection;
+        private readonly ILogger<MongoDbPositionService> _logger;
+
+        public MongoDbPositionService(IMongoCollection<AssetPosition> positionsCollection, ILogger<MongoDbPositionService> logger)
+        {
+            _positionsCollection = positionsCollection;
+            _logger = logger;
+        }
+
+        public async Task SavePositions(IEnumerable<AssetPosition> positions)
+        {
+            if (!positions.Any())
+            {
+                throw new ArgumentException("Should be at least one position", nameof(positions));
+            }
+            await _positionsCollection.InsertManyAsync(positions);
+        }
+    }
+}
---
+++ src/DataMonitoring.MongoDb/MongoSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable 8618
+namespace DataMonitoring.Storage.MongoDb
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+    }
+}
