7b6a43f9923d229ae07037d2ebdf9613a4d0396c
Grigorii Hait <grffn.94@gmail.com>
2021-08-12 16:45:02 +0300

    Add mapper configuration

---
+++ src/DataMonitoring.Worker/EventMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DataMonitoring.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMonitoring.Worker
+{
+    class EventMapperProfile: Profile
+    {
+        public EventMapperProfile()
+        {
+            CreateMap<AssetPositionEvent, AssetPosition>();
+        }
+    }
+}
